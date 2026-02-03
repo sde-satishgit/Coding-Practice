412ce3f6a7bed4c2608356bf657feff808e09c2e
Satish Chavan <satishchavan2102@gmail.com>
2026-02-03 20:58:19 +0530

    copied other java classes

---
+++ Test/src/dto/Employee.java
@@ -0,0 +1,29 @@
+package dto;
+
+public class Employee {
+	
+	private String name;
+	private String empId;
+	private int salary;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	
+
+}
